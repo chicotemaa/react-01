47d37aa0aab0ee033631261016e161da18824c90
matias chicote <chicote.matias@gmail.com>
2022-03-07 10:33:57 -0300

    ejercicio prueba

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Editar <code>src/App.tsx</code> luego gurardar y recargar.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprende React
         </a>
       </header>
     </div>
